7577b8c304cf140594fb4352af224a54c37aa238
Matthew Batchelder <borkweb@gmail.com>
2026-06-17 18:00:08 -0400

    feat(local-site): freeze Jetpack form parity CSS contracts

---
+++ src/lib/replicate/local-site/jetpack-form-css-impl.ts
@@ -0,0 +1,6 @@
+import type { JetpackFormParityCssInput, JetpackFormParityCssResult } from './jetpack-form-css.js';
+
+export function buildJetpackFormParityCssImpl(input: JetpackFormParityCssInput): JetpackFormParityCssResult {
+  void input;
+  return { css: '' };
+}
